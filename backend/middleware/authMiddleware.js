1f5c5198c3a83d307e87bc848fd9df5f26ac45a3
Your Name <you@example.com>
2025-12-14 12:56:10 -0500

    Commit de proyecto completo 2.0

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -12,14 +12,10 @@ export const verifyToken = (req, res, next) => {
 
     try {
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
-
-        req.user = {
-            id_usuario: decoded.id,
-            rol: decoded.rol
-        };
-
+        req.user = decoded; 
         next();
     } catch (err) {
         return res.status(401).json({ message: "Token inválido" });
     }
 };
+
